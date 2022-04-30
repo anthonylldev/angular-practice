821a1c38443ea8546765fca9bc1009e3ae8f52d9
Anthony Fabian Leon Lucero <anthonyfabian22ll@gmail.com>
2022-05-01 00:07:15 +0200

    Implement navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  faUser, faSearch, faShoppingCart,
+  faBars
+} from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  faUser = faUser;
+  faSearch = faSearch;
+  faShoppingCart = faShoppingCart;
+  bars = faBars;
+
+  companyName: string = "Leon-Shopping."
+  user: string = "Username";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
