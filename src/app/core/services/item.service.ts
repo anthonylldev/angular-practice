5798d93142dc5f599b4a13c10fae5dee2e1dcabf
Anthony Fabian Leon Lucero <anthonyfabian22ll@gmail.com>
2022-05-02 08:10:56 +0200

    Add rest to simple item

--- src/app/core/services/item.service.ts
+++ src/app/core/services/item.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import {IItem} from "../interfaces/item.interface";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  public getSimpleItems(): Observable<IItem[]> {
+    const urlEndPoint: string = "http://localhost:3000/catalog/simpleItems";
+    return this.http.get<IItem[]>(urlEndPoint);
+  }
 }
